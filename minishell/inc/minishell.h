e3692e8c0fe26f69de256eca7c5dc03059ea782d
Kevin MARTINS <kemartin@e3r3p18.42.fr>
2019-03-19 18:41:48 +0100

    update header

--- minishell/inc/minishell.h
+++ minishell/inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: kemartin <kemartin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/15 19:27:01 by kemartin          #+#    #+#             */
-/*   Updated: 2019/03/19 15:00:27 by kemartin         ###   ########.fr       */
+/*   Updated: 2019/03/19 18:41:31 by kemartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,16 +51,17 @@ void			exit_function(t_ms *m);
 char			*is_this_home(char *may);
 void			gohome(void);
 void			quote_case(char *str);
+int				spc_cnt(char *str);
 void			get_cmd(t_ms *m);
 int				cmd_not_found(char *cmd, int ret);
 int				var_exe_cmd(t_ms *m, char **env);
 int				is_wp(char *str);
 int				exe_cmd(t_ms *m, char **env);
-int				spc_cnt(char *str);
 void			set_env(char *env, t_ms *m);
+void			freenc(char *name, char *content);
+void			unset_env_while(t_var *af, t_var *prev, t_ms *m, char *del);
 void			unset_env(t_ms *m);
 void			init_env(char **env, t_ms *m);
-void			show_env(t_ms *m, char **env);
 t_var			*ft_create_lst(char *name, char *data);
 void			ft_lst_push_back(t_var **var, char *name, char *data);
 void			ft_lst_clear(t_var **var);
